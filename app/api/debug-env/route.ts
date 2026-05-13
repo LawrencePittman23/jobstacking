049ff5ddfb2fc649d83137b94a8e03dad39cdcd6
LawrencePittman23 <lawrencepittman007@gmail.com>
2026-05-12 23:54:11 -0400

    Extend debug-env to check job API keys

--- app/api/debug-env/route.ts
+++ app/api/debug-env/route.ts
@@ -5,14 +5,14 @@ export const dynamic = "force-dynamic";
 export async function GET() {
   return NextResponse.json({
     has_nextauth_url: !!process.env.NEXTAUTH_URL,
-    nextauth_url_value: process.env.NEXTAUTH_URL || null,
     has_nextauth_secret: !!process.env.NEXTAUTH_SECRET,
     has_google_client_id: !!process.env.GOOGLE_CLIENT_ID,
     has_google_client_secret: !!process.env.GOOGLE_CLIENT_SECRET,
     has_cron_secret: !!process.env.CRON_SECRET,
     has_postgres_url: !!process.env.POSTGRES_URL,
-    node_env: process.env.NODE_ENV,
+    has_adzuna_app_id: !!process.env.ADZUNA_APP_ID,
+    has_adzuna_api_key: !!process.env.ADZUNA_API_KEY,
+    has_rapidapi_key: !!process.env.RAPIDAPI_KEY,
     vercel_env: process.env.VERCEL_ENV,
-    vercel_url: process.env.VERCEL_URL,
   });
 }
